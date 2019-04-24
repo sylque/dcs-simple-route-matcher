0b969463a7af827f8028a332d4004fafa1bdd185
sylque <sylvain.quendez@gmail.com>
2019-04-24 10:33:48 +0200

    Bumped version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sylque:dcs-simple-route-matcher',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary:
     'A Meteor package to match an application route with a Docuss page name',
