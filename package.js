452d505fb29b587c53b6be3879fd4058e6c8afd2
sylque <sylvain.quendez@gmail.com>
2019-05-10 18:00:25 +0200

    Folder structure

--- package.js
+++ package.js
@@ -14,6 +14,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.8.1')
   api.use('ecmascript')
-  api.mainModule('main-client.js', 'client')
-  api.mainModule('main-server.js', 'server')
+  api.mainModule('lib/main-client.js', 'client')
+  api.mainModule('lib/main-server.js', 'server')
 })
