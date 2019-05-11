a6169b74a54bfccf076cf1985eaa3934bc4cd0e3
sylque <sylvain.quendez@gmail.com>
2019-05-11 10:42:57 +0200

    Chnaged required Meteor version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'sylque:dcs-simple-route-matcher',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary:
     'A Meteor package to match an application route with a Docuss page name',
@@ -12,7 +12,7 @@ Package.describe({
 })
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.8.1')
+  api.versionsFrom('1.6.1')
   api.use('ecmascript')
   api.mainModule('lib/main-client.js', 'client')
   api.mainModule('lib/main-server.js', 'server')
